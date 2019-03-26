00c6c2366a8070ea4fb64c887a9d4c813d3f980e
Jann Müller <jann.mueller@lbrm.de>
2019-03-26 09:10:02 +0100

    stylish

--- plutus-contract-exe/src/Language/Plutus/Contract.hs
+++ plutus-contract-exe/src/Language/Plutus/Contract.hs
@@ -47,7 +47,7 @@ import           GHC.Generics (Generic)
 import qualified Ledger.Types as T
 import           Ledger.Value as V
 
--- | An unsigned and potentially unbalanced transaction, as returned by 
+-- | An unsigned and potentially unbalanced transaction, as returned by
 --   a contract endpoint.
 data UnbalancedTx = UnbalancedTx
         { utxInputs  :: [T.TxIn]
@@ -61,32 +61,32 @@ data UnbalancedTx = UnbalancedTx
 -- | Make an unbalanced transaction that does not forge any value.
 mkUnbalancedTx :: [T.TxIn] -> [T.TxOut] -> UnbalancedTx
 mkUnbalancedTx ins outs = UnbalancedTx ins outs V.zero
-    
+
 {- note [Unbalanced transactions]
 
-To turn an 'UnbalancedTx' into a valid transaction that can be submitted to the 
+To turn an 'UnbalancedTx' into a valid transaction that can be submitted to the
 network, the contract backend needs to
 
-* Balance it. 
-  If the total value of `utxInputs` + the `txForge` field is 
+* Balance it.
+  If the total value of `utxInputs` + the `txForge` field is
   greater than the total value of `utxOutput`, then one or more public key
-  outputs need to be added. How many and what addresses they are is up 
-  to the wallet (probably configurable). 
-  If the total balance `utxInputs` + the `txForge` field is less than 
-  the total value of `utxOutput`, then one or more public key inputs need 
+  outputs need to be added. How many and what addresses they are is up
+  to the wallet (probably configurable).
+  If the total balance `utxInputs` + the `txForge` field is less than
+  the total value of `utxOutput`, then one or more public key inputs need
   to be added (and potentially some outputs for the change)
 
 * Compute fees.
-  Once the final size of the transaction is known, the fees for the transaction 
-  can be computed. The transaction fee needs to be paid for with additional 
+  Once the final size of the transaction is known, the fees for the transaction
+  can be computed. The transaction fee needs to be paid for with additional
   inputs so I assume that this step and the previous step will be combined.
 
-  Also note that even if the 'UnbalancedTx' that we get from the contract 
-  endpoint happens to be balanced already, we still need to add fees to it. So 
+  Also note that even if the 'UnbalancedTx' that we get from the contract
+  endpoint happens to be balanced already, we still need to add fees to it. So
   we can't skip the balancing & fee computation step.
 
-* Sign it. 
-  The signing process needs to provide signatures for all public key 
+* Sign it.
+  The signing process needs to provide signatures for all public key
   inputs in the balanced transaction.
 
 
@@ -94,22 +94,22 @@ network, the contract backend needs to
 
 {- note [ContractFinished event]
 
-The 'ContractFinished' event signals that this *instance* of the contract is 
-finished. This means that no more interactions with the instance are possible. 
+The 'ContractFinished' event signals that this *instance* of the contract is
+finished. This means that no more interactions with the instance are possible.
 It is an opportunity for the contract backend to delete all triggers associated
 with the instance, and to update the UI (disable/archive/etc)
 
-Note that the 'ContractFinished' does not say anything about unspent outputs at 
+Note that the 'ContractFinished' does not say anything about unspent outputs at
 the contract address. There may still be unspent outputs at the address, but the
-user cannot do anything with them. 
+user cannot do anything with them.
 
-For example, the game contract emits a 'ContractFinished' event immediately 
-after running 'lock' endpoint. So the contract is finished for the initiator of 
+For example, the game contract emits a 'ContractFinished' event immediately
+after running 'lock' endpoint. So the contract is finished for the initiator of
 the game. Participants in the game may still submit guesses etc.
 
-('ContractFinished' seems to be tied closely to contract roles, and we 
-should think about this some more. This is the difference between the global 
-(ie. blockchain/utxo set) view and the local (what can that particular role do) 
+('ContractFinished' seems to be tied closely to contract roles, and we
+should think about this some more. This is the difference between the global
+(ie. blockchain/utxo set) view and the local (what can that particular role do)
 view of a contract.)
 
 -}
@@ -131,7 +131,7 @@ data ContractOut =
       | ContractFinished
       -- ^ Execution of the contract has ended. No further ledger updates are
       --   required and no user actions are possible. All triggers associated
-      --   with this contract instance can be deleted. See note 
+      --   with this contract instance can be deleted. See note
       --   [ContractFinished event]
 
       deriving stock (Eq, Ord, Show, Generic)
