726be34957452c2565117dbc80d46ac83c3325fe
Jann Müller <jann.mueller@lbrm.de>
2019-03-26 10:28:52 +0100

    _

--- plutus-playground-client/src/Types.purs
+++ plutus-playground-client/src/Types.purs
@@ -56,7 +56,7 @@ _walletId :: Lens' Wallet Int
 _walletId = _Wallet <<< prop (SProxy :: SProxy "getWallet")
 
 _ada :: Lens' Value Int
-_ada = _Value <<< prop (SProxy :: SProxy "getAda")
+_ada = _Value <<< _ <<< gprop (SProxy :: SProxy "getValue")
 
 data Action
   = Action
