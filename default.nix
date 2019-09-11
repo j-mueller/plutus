8610f0cc92766ad6a63bb1c0cbe156df48b07968
Jann Müller <jann.mueller@iohk.io>
2019-09-11 14:19:45 +0200

    binutils

--- default.nix
+++ default.nix
@@ -323,6 +323,7 @@ let
                 ]);
           in  [ 
                 runtimeGhc
+                pkgs.binutils
                 pkgs.coreutils
                 pkgs.bash
                 pkgs.git # needed by cabal-install
